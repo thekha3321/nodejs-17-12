ccc9376e221609c4df773d884255cf6718b477ec
elivedev1 <elivedev1@gmail.com>
2024-12-17 20:27:25 +0700

    second commit

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const port = 8017
 app.get('/', (req, res)=> {
     res.send(`<h1>hello world <h1/>`)
 })
-
 app.listen(port, hostname, () => {
     console.log(`server is running at http://${hostname}:${port}`)
 })
